38732835e960f0c60624ba7aeffa535956762f5b
hectorchanht <hectorchanht@gmail.com>
2024-10-22 03:34:49 -0600

    add realfavicongenerator

--- next.config.mjs
+++ next.config.mjs
@@ -1,21 +1,4 @@
 /** @type {import('next').NextConfig} */
-import FaviconsWebpackPlugin from 'favicons-webpack-plugin';
-
-const nextConfig = {
-  webpack: (config, { isServer }) => {
-    // Add favicons-webpack-plugin to generate favicons
-    if (!isServer) {
-      config.plugins.push(
-        new FaviconsWebpackPlugin({
-          logo: './src/app/favicon.svg',
-          outputPath: 'public/favicons/',
-        })
-      );
-    }
-
-    return config;
-  },
-
-};
+const nextConfig = {};
 
 export default nextConfig;
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
-import Head from 'next/head';
+// import Head from 'next/head';
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -26,9 +26,14 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <Head>
-        <link rel="icon" href="/favicons/favicon.ico" />
-      </Head>
+      <head>
+        <link rel="icon" type="image/png" href="/favicon-48x48.png" sizes="48x48" />
+        <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
+        <link rel="shortcut icon" href="/favicon.ico" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+        <meta name="apple-mobile-web-app-title" content="Fiat Crypto Conversion" />
+        <link rel="manifest" href="/site.webmanifest" />
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
