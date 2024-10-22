b85aa6d21a36ea34c180df00faf8a105f307d0f2
hectorchanht <hectorchanht@gmail.com>
2024-10-21 20:29:01 -0600

    add NextConfig.headers

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,19 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  async headers() {
+    return [
+      {
+        source: '/',
+        headers: [
+          {
+            key: 'x-hello',
+            value: 'there',
+          },
+        ],
+      },
+    ]
+  },
+
+};
 
 export default nextConfig;
