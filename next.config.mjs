02b92873df8621ac03fc5029aae82ea6bee101da
hectorchanht <hectorchanht@gmail.com>
2024-10-22 03:18:56 -0600

    add favicons-webpack-plugin

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,21 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+import FaviconsWebpackPlugin from 'favicons-webpack-plugin';
+
+const nextConfig = {
+  webpack: (config, { isServer }) => {
+    // Add favicons-webpack-plugin to generate favicons
+    if (!isServer) {
+      config.plugins.push(
+        new FaviconsWebpackPlugin({
+          logo: './src/app/favicon.svg',
+          outputPath: 'public/favicons/',
+        })
+      );
+    }
+
+    return config;
+  },
+
+};
 
 export default nextConfig;
