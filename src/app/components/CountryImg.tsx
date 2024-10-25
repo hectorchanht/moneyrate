dc6b4f9525c35bb362ad99e7fd71da960140f59f
hectorchanht <hectorchanht@gmail.com>
2024-10-25 02:16:03 -0600

    hide isImageFound

--- src/app/components/CountryImg.tsx
+++ src/app/components/CountryImg.tsx
@@ -4,13 +4,15 @@ import { currency2country } from '../constants';
 import { QuestionMarkSvg } from '../svgs';
 
 
-const isImageFound = async (uri: string) => {
-  return await fetch(uri, { method: "HEAD", }).then(({ status }) => status === 200);
-};
+// const isImageFound = async (uri: string) => {
+//   return await fetch(uri, { method: "HEAD", }).then(({ status }) => status === 200);
+// };
 
 const CountryImg = ({ code = '', alt = '' }: { code: string, alt?: string }) => {
   const [isError, setIsError] = useState(false);
   const [imageSrc, setImageSrc] = useState<string | null>(null);
+
+  console.log('imageSrc', imageSrc)
 
   useEffect(() => {
     const checkImage = async () => {
@@ -18,7 +20,7 @@ const CountryImg = ({ code = '', alt = '' }: { code: string, alt?: string }) => 
         ? `/country-flags/${currency2country[code.toUpperCase() as keyof typeof currency2country]}.svg`
         : null;
 
-      const cryptoSrc = flagSrc ? null : await isImageFound(`/crypto-icons/${code}.svg`) ? `/crypto-icons/${code}.svg` : `https://assets.coincap.io/assets/icons/${code}@2x.png`;
+      const cryptoSrc = `/crypto-icons/${code}.svg`;
 
       setImageSrc(flagSrc || cryptoSrc);
     };
@@ -26,7 +28,17 @@ const CountryImg = ({ code = '', alt = '' }: { code: string, alt?: string }) => 
     checkImage();
   }, [code]);
 
-  if (isError) return <QuestionMarkSvg class="size-8" width={42} />;
+  if (isError) {
+    if (imageSrc?.includes('/crypto-icons')) {  // if not found as crypto
+      return <Image className="w-[42px] flex-shrink-0"
+        height={42} width={42}
+        alt={alt ?? code}
+        src={`https://assets.coincap.io/assets/icons/${code}@2x.png`}
+        onError={() => setIsError(true)}
+      />
+    }
+    return <QuestionMarkSvg className="size-8" width={42} />;
+  }
 
   return (
     <Image className="w-[42px] flex-shrink-0"
