103ad498d686e57a86862cfe102c0ed6011b54fc
hectorchanht <hectorchanht@gmail.com>
2024-11-07 04:19:13 -0700

    fix err

--- src/app/chart/page.tsx
+++ src/app/chart/page.tsx
@@ -1,37 +1,39 @@
 "use client";
 
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { CartesianGrid, Line, LineChart, ResponsiveContainer, Tooltip, XAxis, YAxis } from 'recharts';
 import useSWR from 'swr';
 import { fetcher } from '../api';
 
 const CurrencyChart = () => {
   const [timeframe, setTimeframe] = useState<'Daily' | 'Weekly' | 'All'>('Daily');
+  const [q, setQ] = useState<string | null>(null);
 
-  if (typeof window === undefined) return null;
-  const params = new URLSearchParams(window.location.search);
-  const q = params.get('q')?.toUpperCase();
+  useEffect(() => {
+    const params = new URLSearchParams(window.location.search);
+    setQ(params.get('q')?.toUpperCase() || null);
+  }, []);
 
-  const { data, error } = useSWR(`/api/currencyChart?q=${q}`, fetcher, { keepPreviousData: true });
+  const { data, error } = useSWR(q ? `/api/currencyChart?q=${q}` : null, fetcher, { keepPreviousData: true });
 
-  if (error) return <div>Error loading data</div>;
+  if (error || !q) return <div className="text-center">Error loading data</div>;
   if (!data) return <progress className="progress w-full mt-[2px]"></progress>;
 
   return (
-    <div className="w-full h-dvh p-[12px] bg-[black]">
+    <div className="w-full h-[100vh] overflow-auto p-[12px] bg-[black] container mx-auto">
       <div className="flex justify-center">{q}</div>
       <div className="flex justify-center mb-4">
         <button onClick={() => setTimeframe('Daily')} className="m-4 border p-4 rounded-full">Daily</button>
         <button onClick={() => setTimeframe('Weekly')} className="m-4 border p-4 rounded-full">Weekly</button>
         <button onClick={() => setTimeframe('All')} className="m-4 border p-4 rounded-full">All</button>
       </div>
-      <ResponsiveContainer>
-        <LineChart data={data[timeframe]}>
+      <ResponsiveContainer width="100%" height="70%" >
+        <LineChart data={data[timeframe]} >
           <CartesianGrid strokeDasharray="3 3" />
-          <XAxis dataKey="time" />
+          <XAxis dataKey="time" orientation='top' />
           <YAxis />
           <Tooltip />
-          <Line type="natural" dataKey="value" stroke="#8884d8" />
+          <Line type="natural" dataKey="value" stroke="" />
         </LineChart>
       </ResponsiveContainer>
     </div>
