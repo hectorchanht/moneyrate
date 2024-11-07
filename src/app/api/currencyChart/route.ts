fd7f42cd65fb424a1ed0b28ca7b73434805d9db3
hectorchanht <hectorchanht@gmail.com>
2024-11-07 15:23:16 -0700

    add time slider for chart , more api return

--- src/app/api/currencyChart/route.ts
+++ src/app/api/currencyChart/route.ts
@@ -1,44 +1,65 @@
 import { NextResponse } from 'next/server';
 
+// // mtfxgroup have less data point so it get replaced
+// const response = await fetch(`https://www.mtfxgroup.com/api/rates/getHistoricalData/?ratepair=${ratepair}`);
+// const data = await response.json();
+// const parse = (data: [number, number][]) => data.map(([timestamp, value]) => ({
+//   time: new Date(timestamp).toLocaleDateString(), // Format timestamp for X-axis
+//   value,
+// }));
+// data.Daily = parse(data?.DailyData);
+// data.Weekly = parse(data?.WeeklyData);
+// data.All = parse(data?.AllData);
+
 
 export async function GET(request: Request) {
   const { searchParams } = new URL(request.url);
   const ratepair = searchParams.get('q');
+  // const start = searchParams.get('start') ?? 1;
+  // const end = searchParams.get('end') ?? + new Date();
+  // no need to have start end as data display will be filter in front end
 
   if (!ratepair) {
-    return NextResponse.json({ error: 'Please provide a query parameter "q" (e.g., /currencyChart/q=USDCAD) to get the currency data.' }); // User-friendly error message
+    return NextResponse.json({ error: `Please provide a query parameter 'q' (e.g., /currencyChart/q=USD-CAD) to get the currency chart data.` }); // User-friendly error message
   }
 
+  let
+    targetCur = ratepair?.split('-')[0].toUpperCase(),
+    baseCur = ratepair?.split('-')[1].toUpperCase(),
+    data,
+    is_flip = false;
 
-  // // mtfxgroup have less data point
-  // const response = await fetch(`https://www.mtfxgroup.com/api/rates/getHistoricalData/?ratepair=${ratepair}`);
-  // const data = await response.json();
-  // const parse = (data: [number, number][]) => data.map(([timestamp, value]) => ({
-  //   time: new Date(timestamp).toLocaleDateString(), // Format timestamp for X-axis
-  //   value,
-  // }));
-  // data.Daily = parse(data?.DailyData);
-  // data.Weekly = parse(data?.WeeklyData);
-  // data.All = parse(data?.AllData);
+  const [r_fiat, r_crypto, r_crypto_usd, r_crypto_flip] = await Promise.all([
+    fetch(`https://query2.finance.yahoo.com/v8/finance/chart/${ratepair.replace('-', '')}=X?period1=0&period2=${+ new Date()}&interval=1mo&includePrePost=true`),
+    fetch(`https://query1.finance.yahoo.com/v8/finance/chart/${ratepair}?period1=0&period2=${+ new Date()}&interval=1mo&includePrePost=true`),
+    fetch(`https://query1.finance.yahoo.com/v8/finance/chart/${targetCur + '-' + 'USD'}?period1=0&period2=${+ new Date()}&interval=1mo&includePrePost=true`),
+    fetch(`https://query1.finance.yahoo.com/v8/finance/chart/${baseCur + '-' + targetCur}?period1=0&period2=${+ new Date()}&interval=1mo&includePrePost=true`),
+  ]);
 
-  let data, response;
+  const [data_fiat, data_crypto, data_crypto_usd, data_crypto_flip] = await Promise.all([
+    r_fiat.json(), r_crypto.json(), r_crypto_usd.json(), r_crypto_flip.json(),
+  ]);
 
-  response = await fetch(`https://query2.finance.yahoo.com/v8/finance/chart/${ratepair.replace('-', '')}=X?period1=0&period2=${+ new Date()}&interval=1mo&includePrePost=true`);
-
-  data = await response.json();
-
-  if (!data?.chart?.result) {
-    response = await fetch(`https://query1.finance.yahoo.com/v8/finance/chart/${ratepair}?period1=0&period2=${+ new Date()}&interval=1mo&includePrePost=true`);
-
-    data = await response.json();
+  if (data_fiat?.chart?.result?.[0]) {
+    data = data_fiat;
+  } else if (data_crypto?.chart?.result?.[0]) {
+    data = data_crypto;
+  } else if (data_crypto_usd?.chart?.result?.[0]) {
+    data = data_crypto_usd;
+    baseCur = 'USD'
+  } else if (data_crypto_flip?.chart?.result?.[0]) {
+    data = data_crypto_flip;
+    is_flip = true;
   }
-
 
   const { timestamp, indicators } = data?.chart?.result?.[0];
   const { close } = indicators?.quote?.[0];
 
-  return NextResponse.json(timestamp.map((t: number, i: number) => ({
-    time: new Intl.DateTimeFormat('en-GB').format(new Date(t * 1000)), // Format timestamp to dd/mm/yyyy
-    value: close[i]
-  })));
+  const chartData = timestamp.map((t: number, i: number) => ({
+    date: new Intl.DateTimeFormat('en-GB').format(new Date(t * 1000)), // Format timestamp to dd/mm/yyyy
+    value: is_flip ? (1 / close[i]) : close[i],
+    timestamp: t,
+  }));
+
+  return NextResponse.json({ data: chartData, title: `1 ${targetCur} = ? ${baseCur}` });
 } 
--- src/app/chart/page.tsx
+++ src/app/chart/page.tsx
@@ -5,8 +5,17 @@ import { CartesianGrid, Line, LineChart, ResponsiveContainer, Tooltip, XAxis, YA
 import useSWR from 'swr';
 import { fetcher } from '../api';
 
+// Define the type for the data items
+interface DataItem {
+  date: string;
+  timestamp: number;
+  value: number; // Add other properties as needed
+}
+
 const CurrencyChart = () => {
   const [q, setQ] = useState<string | null>(null);
+  const [startTimestamp, setStartTimestamp] = useState<number>(0);
+  const [endTimestamp, setEndTimestamp] = useState<number>(0);
 
   useEffect(() => {
     const params = new URLSearchParams(window.location.search);
@@ -18,17 +27,46 @@ const CurrencyChart = () => {
   if (!!error) return <div className="text-center">Error loading data</div>;
   if (!data || !q) return <progress className="progress w-full mt-[2px]"></progress>;
 
+  // Set default start and end timestamps based on fetched data
+  if (startTimestamp === 0 && data.data.length > 0) {
+    setStartTimestamp(data.data[0].timestamp);
+    setEndTimestamp(data.data[data.data.length - 1].timestamp);
+  }
+
+  // Filter the data based on the selected timestamps
+  const filteredData = data.data.filter((item: DataItem) => item.timestamp >= startTimestamp && item.timestamp <= endTimestamp);
+
   return (
     <div className="w-full h-[100vh] overflow-auto pt-[20px] container mx-auto">
-      <div className="flex justify-center text-[40px]">1 {q?.split('-')[0]} = ? {q?.split('-')[1]}</div>
+      <div className="flex justify-center text-[40px]">{data?.title}</div>
 
-      <ResponsiveContainer width="100%" height="70%" >
-        <LineChart data={data} >
+      {/* Range slider for selecting start and end timestamps */}
+      <div className="flex justify-center mb-4">
+        <input
+          type="range"
+          min={data.data[0]?.timestamp}
+          max={endTimestamp}
+          value={startTimestamp}
+          onChange={(e) => setStartTimestamp(Number(e.target.value))}
+          className="slider bg-white"
+        />
+        <input
+          type="range"
+          min={startTimestamp}
+          max={data.data[data.data.length - 1]?.timestamp}
+          value={endTimestamp}
+          onChange={(e) => setEndTimestamp(Number(e.target.value))}
+          className="slider"
+        />
+      </div>
+
+      <ResponsiveContainer width="100%" height="70%">
+        <LineChart data={filteredData}>
           <CartesianGrid strokeDasharray="4 2 0" />
-          <XAxis dataKey="time" orientation='top' />
+          <XAxis dataKey="date" domain={['dataMin', 'dataMax']} />
           <YAxis />
-          <Tooltip labelStyle={{ color: 'black' }} contentStyle={{ background: 'white' }} itemStyle={{ fontWeight: '700' }} formatter={(value) => [value]} />
-          <Line type="natural" dataKey="value" stroke="" />
+          <Tooltip labelStyle={{ color: 'black' }} contentStyle={{ background: 'white' }} itemStyle={{ fontWeight: '700', color: 'black' }} formatter={(value) => [value]} />
+          <Line type="monotone" dataKey="value" stroke="white" />
         </LineChart>
       </ResponsiveContainer>
     </div>
