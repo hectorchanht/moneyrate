153fa667d405d399166353c78afc97ef0e1670bb
hectorchanht <hectorchanht@gmail.com>
2024-11-07 04:33:15 -0700

    show err

--- src/app/chart/page.tsx
+++ src/app/chart/page.tsx
@@ -16,8 +16,8 @@ const CurrencyChart = () => {
 
   const { data, error } = useSWR(q ? `/api/currencyChart?q=${q}` : null, fetcher, { keepPreviousData: true });
 
-  if (!data) return <progress className="progress w-full mt-[2px]"></progress>;
-  if (error || !q) return <div className="text-center">Error loading data</div>;
+  if (!!error) return <div className="text-center">Error loading data</div>;
+  if (!data || !q) return <progress className="progress w-full mt-[2px]"></progress>;
 
   return (
     <div className="w-full h-[100vh] overflow-auto pt-[20px] bg-[black] container mx-auto">
