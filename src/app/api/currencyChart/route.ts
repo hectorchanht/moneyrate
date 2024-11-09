434ec321b386dbf860bb6931104f0282cccf18c3
hectorchanht <hectorchanht@gmail.com>
2024-11-08 22:54:41 -0700

    remove data_crypto_usd which change base to usd

--- src/app/api/currencyChart/route.ts
+++ src/app/api/currencyChart/route.ts
@@ -14,7 +14,7 @@ import { NextResponse } from 'next/server';
 
 export async function GET(request: Request) {
   const { searchParams } = new URL(request.url);
-  const ratepair = searchParams.get('q');
+  const ratepair = searchParams.get('q'); // it is [targetCur]-[baseCur]
   // const start = searchParams.get('start') ?? 1;
   // const end = searchParams.get('end') ?? + new Date();
   // no need to have start end as data display will be filter in front end
@@ -29,24 +29,24 @@ export async function GET(request: Request) {
     data,
     is_flip = false;
 
-  const [r_fiat, r_crypto, r_crypto_usd, r_crypto_flip] = await Promise.all([
-    fetch(`https://query2.finance.yahoo.com/v8/finance/chart/${ratepair.replace('-', '')}=X?period1=0&period2=${+ new Date()}&interval=1mo&includePrePost=true`),
-    fetch(`https://query1.finance.yahoo.com/v8/finance/chart/${ratepair}?period1=0&period2=${+ new Date()}&interval=1mo&includePrePost=true`),
-    fetch(`https://query1.finance.yahoo.com/v8/finance/chart/${targetCur + '-' + 'USD'}?period1=0&period2=${+ new Date()}&interval=1mo&includePrePost=true`),
-    fetch(`https://query1.finance.yahoo.com/v8/finance/chart/${baseCur + '-' + targetCur}?period1=0&period2=${+ new Date()}&interval=1mo&includePrePost=true`),
+  const getApiUri = (ratepair: string) => {
+    return `https://query1.finance.yahoo.com/v8/finance/chart/${ratepair}?period1=0&period2=${+ new Date()}&interval=1mo&includePrePost=true`;
+  }
+
+  const [r_fiat, r_crypto, r_crypto_flip] = await Promise.all([
+    fetch(getApiUri(ratepair.replace('-', '') + '=X')),
+    fetch(ratepair),
+    fetch(baseCur + '-' + targetCur),
   ]);
 
-  const [data_fiat, data_crypto, data_crypto_usd, data_crypto_flip] = await Promise.all([
-    r_fiat.json(), r_crypto.json(), r_crypto_usd.json(), r_crypto_flip.json(),
+  const [data_fiat, data_crypto, data_crypto_flip] = await Promise.all([
+    r_fiat.json(), r_crypto.json(), r_crypto_flip.json(),
   ]);
 
   if (data_fiat?.chart?.result?.[0]) {
     data = data_fiat;
   } else if (data_crypto?.chart?.result?.[0]) {
     data = data_crypto;
-  } else if (data_crypto_usd?.chart?.result?.[0]) {
-    data = data_crypto_usd;
-    baseCur = 'USD'
   } else if (data_crypto_flip?.chart?.result?.[0]) {
     data = data_crypto_flip;
     is_flip = true;
